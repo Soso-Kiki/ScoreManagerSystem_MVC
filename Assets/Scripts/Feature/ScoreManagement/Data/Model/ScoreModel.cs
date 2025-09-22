c6632ac72a2661490657ee2f3170f9df6e2a3dd4
Soso-Kiki <polytechid10@gmail.com>
2025-09-22 09:32:46 +0900

    Update ScoreModel.cs

--- Assets/Scripts/Feature/ScoreManagement/Data/Model/ScoreModel.cs
+++ Assets/Scripts/Feature/ScoreManagement/Data/Model/ScoreModel.cs
@@ -5,10 +5,9 @@ namespace Feature.ScoreManagement.Data.Model
 {
     public class ScoreModel //: //MonoBehaviour
     {
-        // 점수가 변경되었음을 알리는 이벤트
-        //public event Action<int> OnScoreChanged;
+      
 
-        // 실제 점수 값 (private으로 보호)
+        // 실제 점수 값 
         private int _score;
 
         // 외부에서 점수 값에 접근하고 설정하는 프로퍼티
@@ -18,9 +17,8 @@ namespace Feature.ScoreManagement.Data.Model
             set
             {
                 _score = value;
-                // 점수 값이 변경되면 OnScoreChanged 이벤트를 호출하여
-                // 구독자(View 등)에게 새로운 점수 값을 알려줍니다.
-                //OnScoreChanged?.Invoke(_score);
+                // 점수 값이 변경되면 OnScoreChanged 이벤트를 호출
+                
             }
         }
 
